8b3b033d76a3761bf888c4d2f0d97af6f6d2768f
Igor-Moraes <igorcdemoraes@gmail.com>
2026-03-19 16:34:47 -0300

    Add files via upload

--- php/admin.php
+++ php/admin.php
@@ -2,18 +2,74 @@
 
 require 'security.php';
 
-echo "usuário logado";
+
 
 ?>
 <!doctype html>
 <html>
 <header>
-	<title>Login</title>
+	<title>Bem vindo !</title>
 	<meta lang="pt-br">
     <meta charset="UTF-8">
+	    <!-- Favicons -->
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="48x48" href="favicon-48x48.png">
+    <link rel="icon" type="image/png" sizes="64x64" href="favicon-64x64.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon-180x180.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="favicon-512x512.png">
+    <link rel="icon" href="favicon.ico">
+    <link rel="stylesheet" href="style.css"> <!--Conecta o HTML ao CSS -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="app.js" defer ></script>
+	<link rel="stylesheet" href="..\mobile.css">
+	<link rel="stylesheet" href="css/login.css">
+	<script src="..\app.js" defer></script>
 	<!--link rel="stylesheet" href="css/estilo.css" -->
 </header>
 <body>
-	<a href="logout.php">Sair</a>
+	<nav class="menu-desktop">
+        <ul>
+            <li><img src="images/rodrigo.jpg" alt="Foto do Usuário " id="usuario"></li> <!--Logo da empresa no menu-->
+            <!--Links do menu-->
+			<li><h2>Bem vindo! Rodrigo Pereira Silva</h2></li>
+            <a href="logout.php" id="btn-entrar" style="margin-top: 18px; font-family: 'Roboto', sans-serif; font-size: 18px;">Sair</a>
+        </ul>
+
+    </nav>
+
+	<!--Menu Mobile -->
+            <div class="mobile">
+                <img src="images/rodrigo.jpg" alt="Foto do Usuário " id="logo-mobile"> <!--Logo da empresa no menu-->
+
+                           <div class="btn-abrir-menu" id="btn-menu">
+                    <img src="images/list.svg" alt="Icone de lista"> <!--btn-menu-->
+                </div>
+            </div>
+            <div class="menu-mobile" id="menu-mobile">
+                <div class="btn-fechar">
+                    <img src="images/x-lg.svg" alt="Fechar menu">
+                   <a href="logout.php" id="btn-mobile">Sair</a>
+
+                </div> <!--btn-fechar-->
+
+                <nav>
+        <ul>
+		
+            <!--Links do menu-->
+           
+        </ul>
+                </nav>
+
+            </div> <!--menu-mobile-->
+
+            <div class="overlay-menu" >
+
+            </div>
+    <!--Menu interativo-->
+	
+	
+	
 </body>
 </html>
--- php/login.php
+++ php/login.php
@@ -9,7 +9,7 @@
     <link rel="icon" type="image/png" sizes="512x512" href="favicon-512x512.png">
     <link rel="icon" href="favicon.ico">
     <link href="../mobile.css" rel="stylesheet">
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="css/login.css">
     <script src="../app.js" defer></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,43 +19,42 @@
 
 <nav class="menu-desktop">
         <ul>
-            <li><img src="imagens/logo.png" alt="Logo da empresa, um desenho de bíceps com fundo vermelho em círculo"></li> <!--Logo da empresa no menu-->
+            <li><img src="images/logo.png" alt="Logo da empresa, um desenho de bíceps com fundo vermelho em círculo"></li> <!--Logo da empresa no menu-->
             <!--Links do menu-->
-            <li> <a href="index.html">Início</a> </li>
+            <li> <a href="..\index.html">Início</a> </li>
             <li>
                 <a href="#Serviços">Serviços</a>
                 <ul>
-                    <li> <a href="produtos.html">Produtos</a> </li>
-                    <li> <a href="musc.html">Musculação</a> </li>
-                    <li> <a href="cross.html">Cross fit</a> </li>
-                    <li> <a href="yoga.html">Yoga</a> </li>
-                    <li><a href="nutrição.html">Nutrição</a></li>
-                    <li><a href="login.php">Login</a></li>
+                    <li> <a href="..\produtos.html">Produtos</a> </li>
+                    <li> <a href="..\musc.html">Musculação</a> </li>
+                    <li> <a href="..\cross.html">Cross fit</a> </li>
+                    <li> <a href="..\yoga.html">Yoga</a> </li>
+                    <li><a href="..\nutrição.html">Nutrição</a></li>
                 </ul>
             </li>
             <li>
-                <a href="contato.html">Contatos</a>
+                <a href="..\contato.html">Contatos</a>
             </li>
             <li>
-                <a href="experimental.html">Aula experimental</a>
+                <a href="..\experimental.html">Aula experimental</a>
             </li>
-            <li> <a href="sobre.html">Sobre</a></li>
-            <li> <a href="faq.html">FAQ</a></li>
-            <a href="#" id="btn-entrar">Entrar</a>
+            <li> <a href="..\sobre.html">Sobre</a></li>
+            <li> <a href="..\faq.html">FAQ</a></li>
+            <a href="login.php" id="btn-entrar">Entrar</a>
         </ul>
 
     </nav>
 
             <div class="mobile">
-                <img src="imagens/logo.png" alt="Logo da empresa, um desenho de bíceps com fundo vermelho em círculo" id="logo-mobile"> <!--Logo da empresa no menu-->
+                <img src="images/logo.png" alt="Logo da empresa, um desenho de bíceps com fundo vermelho em círculo" id="logo-mobile"> <!--Logo da empresa no menu-->
 
                            <div class="btn-abrir-menu" id="btn-menu">
-                    <img src="imagens/list.svg" alt="Icone de lista"> <!--btn-menu-->
+                    <img src="images/list.svg" alt="Icone de lista"> <!--btn-menu-->
                 </div>
             </div>
             <div class="menu-mobile" id="menu-mobile">
                 <div class="btn-fechar">
-                    <img src="imagens/x-lg.svg" alt="Fechar menu">
+                    <img src="images/x-lg.svg" alt="Fechar menu">
                     <a href="login.php" id="btn-mobile">Entrar</a>
 
                 </div> <!--btn-fechar-->
@@ -63,25 +62,25 @@
                 <nav>
         <ul>
             <!--Links do menu-->
-            <li> <a href="index.html">Início</a> </li>
+            <li> <a href="..\index.html">Início</a> </li>
             <li>
                 <a href="#Serviços">Serviços</a>
                 <ul>
-                    <li> <a href="produtos.html">Produtos</a> </li>
-                    <li> <a href="musc.html">Musculação</a> </li>
-                    <li> <a href="cross.html">Cross fit</a> </li>
-                    <li> <a href="yoga.html">Yoga</a> </li>
-                    <li><a href="nutrição.html">Nutrição</a></li>
+                    <li> <a href="..\produtos.html">Produtos</a> </li>
+                    <li> <a href="..\musc.html">Musculação</a> </li>
+                    <li> <a href="..\cross.html">Cross fit</a> </li>
+                    <li> <a href="..\yoga.html">Yoga</a> </li>
+                    <li><a href="..\nutrição.html">Nutrição</a></li>
                 </ul>
             </li>
             <li>
-                <a href="contato.html">Contatos</a>
+                <a href="..\contato.html">Contatos</a>
             </li>
             <li>
-                <a href="experimental.html">Aula experimental</a>
+                <a href="..\experimental.html">Aula experimental</a>
             </li>
-            <li> <a href="sobre.html">Sobre</a></li>
-            <li> <a href="faq.html">FAQ</a></li>
+            <li> <a href="..\sobre.html">Sobre</a></li>
+            <li> <a href="..\faq.html">FAQ</a></li>
         </ul>
                 </nav>
 
